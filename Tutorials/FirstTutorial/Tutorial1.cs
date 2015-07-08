f202f2223b00b0d0cce2e91606412ba7aeafd362
chronium <onlivechronium@gmail.com>
2015-07-08 17:55:41 +0300

    Added some comments to the tutorial.
    Signed-off-by: chronium <onlivechronium@gmail.com>

--- Tutorials/FirstTutorial/Tutorial1.cs
+++ Tutorials/FirstTutorial/Tutorial1.cs
@@ -7,15 +7,21 @@ using ChronosEngine;
 using OpenTK;
 
 namespace Tutorials.FirstTutorial {
+	// Inherit from the base class ChronoGame
 	public class Tutorial1: ChronoGame {
+		// Required call to the base constructor. 
+		// It sets up the window and an orthographic projection.
 		public Tutorial1() : base("Tutorial 1 - First window") { }
 
 		public override void OnLoad(EventArgs e) {
+			// The base function generates an asset context and a rendering context.
 			base.OnLoad(e);
 		}
 
 		public override void OnRenderFrame(FrameEventArgs e) {
+			// We clear the back buffer to have a clean frame.
 			this.Clear();
+			// We swap the back and front buffer to present our frame to the user.
 			this.SwapBuffers();
 		}
 	}
