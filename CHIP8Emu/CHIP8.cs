a7bdc257f7dc94a6b5c5addea1ba39ab4904c8d9
chronium <onlivechronium@gmail.com>
2015-07-09 14:14:37 +0300

    Sneak peek on an upcoming project to test the engine.
    Signed-off-by: chronium <onlivechronium@gmail.com>

---
+++ CHIP8Emu/CHIP8.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChronosEngine;
+using ChronosEngine.Structures;
+using OpenTK;
+
+namespace CHIP8Emu {
+	public class CHIP8 : ChronoGame {
+		public int Scale { get; set; }
+		public Resolution ScreenSize { get; set; }
+
+		public CHIP8(int scale = 16, int width = 64, int height = 32) 
+			: base(new Resolution(width * scale, height * scale), "CHIP8 Emulator") {
+			this.Scale = scale;
+			this.ScreenSize = new Resolution(width, height);
+		}
+
+		public override void OnUpdateFrame(FrameEventArgs e) {
+			base.OnUpdateFrame(e);
+		}
+
+		public override void OnLoad(EventArgs e) {
+			this.Clear();
+			this.SwapBuffers();
+		}
+	}
+}
---
+++ CHIP8Emu/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CHIP8Emu {
+	class Program {
+		static void Main(string[] args) {
+			new CHIP8().Run();
+		}
+	}
+}
